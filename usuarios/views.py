7459988d0f93e4231b891e9e1386d3ae76c3b790
repolca <therepolca@gmail.com>
2025-02-20 22:31:19 -0500

    Registro y modificacion views usuario

---
+++ usuarios/views.py
@@ -0,0 +1,16 @@
+
+#Esto es del sign up
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # Redirige al login después del registro
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "signup.html", {"form": form})
